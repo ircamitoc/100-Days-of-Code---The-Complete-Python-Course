e5e692ed28cd96d245d4dc34ddd8246a0830aee1
Isaiah Rain R. Camitoc <81004648+ircamitoc@users.noreply.github.com>
2023-02-02 21:30:04 +0800

    Create Python Replit- Day 3 Challenge.py

---
+++ Python Replit- Day 3 Challenge.py
@@ -0,0 +1,9 @@
+food = input("Name a type of food > ")
+plant = input("Name a plant > ")
+cooking = input("What is a way to cook something > ")
+burned_food = input("How do you describe burnt food? > ")
+diy = input("Name something in your house > ")
+
+print("\nTonight's dinner:")
+
+print("For dinner, you should make",cooking, food, "with", burned_food, plant, "on a bed of", diy + "s")
