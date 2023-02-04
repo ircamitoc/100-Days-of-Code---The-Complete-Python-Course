dda2ea01b9eb87f841ee91922e0f5dac303f2795
Isaiah Rain R. Camitoc <81004648+ircamitoc@users.noreply.github.com>
2023-02-04 11:06:37 +0800

    Create Python Replit - Day 16 Challenge.py

---
+++ Python Replit - Day 16 Challenge.py
@@ -0,0 +1,14 @@
+counter = 1
+
+while True:
+  lyric = input("Never going to _____ you up. > ")
+  if lyric == "give" or lyric == "Give":
+    print("You got it")
+  else:
+    print("Nope, try again")
+    counter += 1
+  if lyric == "give":
+    break
+
+print("You got the correct lyrics in", counter, "attempt(s).")
+  
