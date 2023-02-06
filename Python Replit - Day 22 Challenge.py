d63efaccbc5ab81d733be190f6b8ffcac37e1eb3
Isaiah Rain R. Camitoc <81004648+ircamitoc@users.noreply.github.com>
2023-02-06 21:11:48 +0800

    Create Python Replit - Day 22 Challenge.py

---
+++ Python Replit - Day 22 Challenge.py
@@ -0,0 +1,27 @@
+import random
+
+number = random.randint(1,1000000)
+counter = 1
+
+print("Number Guesser")
+print("---")
+while True:
+    answer = int(input("What is your guess: "))
+    if answer < 0:
+      print("Please enter positive values.")
+      exit()
+    elif answer < number:
+        counter += 1
+        print("Too low. Try again.\n")
+    elif answer > number:
+        counter += 1
+        print("Too high. Try again.\n")
+        continue
+    elif answer == number:
+        print("Correct!\n")
+        counter += 1
+        print("\nIt took you", counter, "guesses to get it correct!")
+        break
+        exit()
+    else:
+        print("That is not a number I recognize.")
