0af52833998b4a96095eccde592efbc25a2e0cb8
Isaiah Rain R. Camitoc <81004648+ircamitoc@users.noreply.github.com>
2023-02-06 21:06:20 +0800

    Update Python Replit - Day 21 Challenge.py

--- Python Replit - Day 21 Challenge.py
+++ Python Replit - Day 21 Challenge.py
@@ -1,5 +1,30 @@
-loan = 1000
-for counter in range(10):
-  loan += (loan*0.05)
-  print("Year", counter+1, ":", round(loan,2))
-  
+print("Math Game")
+print("---")
+multiples = int(input(("Name your multiples > ")))
+end = multiples*11
+counter = 0
+score = 0
+
+for x in range(1, 11):
+  correct_answer = x * multiples    
+  print()
+  print(x,"X",multiples)
+  answer = int(input("Answer > "))
+  if answer == correct_answer:
+    counter += 1
+    score += 1
+    print("Great work! 🥳")
+    continue
+  elif answer != correct_answer:
+    counter += 1
+    print("""Nope! 😭 The answer was""", x)
+    continue
+  else:
+    print("Invalid input. Please try again.\n")
+
+if counter == 10:
+  print("\nYou got a perfect score !")
+  exit()
+else:
+  print("\nYou scored", score, "out of 10!")
+  exit()
